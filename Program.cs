74f7455ac969be2c04c665ee25328b17fcc43cc5
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-17 18:19:10 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //a reg while loop:
+            //int index = 1;
+
+            //while (index <= 5)
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+            //}
+
+            //a do while loop. a do while loop will check the code before it checks the condition
+            //int index = 6;
+
+            //do 
+            //{
+            //    Console.WriteLine(index);
+            //    index++;
+            //} while (index <= 5) ;
+
+            //    Console.ReadLine();
+        }
+    }
+}
